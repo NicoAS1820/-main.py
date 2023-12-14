84fb3f5d5c16492eb6269fe536013177c8a68fcf
NicoAS1820 <153239802+NicoAS1820@users.noreply.github.com>
2023-12-14 19:01:15 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            "ROFL": "una respuesta a una broma",
+            "SHEESH": "ligera desaprobación",
+            "CREEPY": "aterrador, siniestro",
+            "AGGRO": "ponerse agresivo/enojado"
+    
+}
+
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+
+
+if word in meme_dict.keys():
+    print(meme_dict[word])
+    
+else:
+    print("No tengo esa palabra registrada ):<")
+    # ¿Qué hacer si no se encuentra la palabra?
